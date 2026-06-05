e52b69ed524815a48405a22fe82914b01eed9989
Greenmarchant <karanjatrevor1@gmail.com>
2026-06-06 01:11:32 +0300

    Added Task 1,Task2,Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args )
+    {
+        Console.Write("Enter a starting number (will be maximized at 5): ");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+
+        // Ensure the user input does not exceed 5
+        if (userInput > 5)
+            userInput = 5;
+
+        int counter = 0;
+
+        do
+        {
+            Console.WriteLine(counter);
+            counter++;
+        }
+        while (counter <= userInput);
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int number = 0;
+
+        while (number <= 10)
+        {
+            Console.WriteLine(number);
+            number++;
+        }
+    }
+}
